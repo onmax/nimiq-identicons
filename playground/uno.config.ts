a518e2069771ef65caf340784ad811c41c23ec82
onmax <maximogarciamtnez@gmail.com>
2025-04-25 18:50:29 +0200

    chore: simplify development scripts and update package dependencies to use catalog references

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,26 +1,16 @@
-import { createExternalPackageIconLoader } from '@iconify/utils/lib/loader/external-pkg'
 import { presetNimiq } from 'nimiq-css'
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
-import { presetFluidSizing } from 'unocss-preset-fluid-sizing'
+import { defineConfig } from 'unocss'
+import { presetOnmax } from 'unocss-preset-onmax'
 import { presetScalePx } from 'unocss-preset-scale-px'
 
 export default defineConfig({
-  // ...UnoCSS options
   presets: [
-    presetUno(),
-    presetAttributify(),
+    presetOnmax({ presets: { wind4: false } }),
     presetNimiq({
       attributifyUtilities: true,
       utilities: true,
       staticContent: true,
     }),
-    presetIcons({
-      collections: {
-        ...createExternalPackageIconLoader('@iconify-json/nimiq'),
-      },
-    }),
-    // @ts-expect-error The preset is fine
     presetScalePx(),
-    presetFluidSizing(),
   ],
 })
